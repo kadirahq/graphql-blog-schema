d247559d9189b384de94b38587080bf2be71e0da
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-10-05 18:28:25 +0530

    Add the basic schema

--- src/schema.js
+++ src/schema.js
@@ -1,30 +1,129 @@
+import PostsList from './data/posts';
+import AuthorsMap from './data/authors';
+import * as _ from 'underscore';
+
 import {
+  GraphQLList,
   GraphQLObjectType,
   GraphQLSchema,
-  GraphQLInt
+  GraphQLString,
+  GraphQLInt,
+  GraphQLFloat,
+  GraphQLEnumType
 } from 'graphql';
 
-let count = 0;
+const Category = new GraphQLEnumType({
+  name: "Category",
+  description: "A Category of the blog",
+  values: {
+    METEOR: {value: "meteor"},
+    PRODUCT: {value: "product"},
+    USER_STORY: {value: "user-story"},
+    OTHER: {value: 'other'}
+  }
+});
 
-let Schema = new GraphQLSchema({
+const Author = new GraphQLObjectType({
+  name: "Author",
+  description: "Represent the type of an author of a blog post or a comment",
+  fields: () => ({
+    _id: {type: GraphQLString},
+    name: {type: GraphQLString},
+    twitterHandle: {type: GraphQLString}
+  })
+});
+
+const Comment = new GraphQLObjectType({
+  name: "Comment",
+  description: "Represent the type of a comment",
+  fields: () => ({
+    _id: {type: GraphQLString},
+    content: {type: GraphQLString},
+    author: {
+      type: Author,
+      resolve: function({author}) {
+        return AuthorsMap[author];
+      }
+    },
+    timestamp: {type: GraphQLFloat},
+    replies: {
+      type: new GraphQLList(Comment),
+      description: "Replies for the comment"
+    }
+  })
+});
+
+const Post = new GraphQLObjectType({
+  name: "Post",
+  description: "Represent the type of a blog post",
+  fields: () => ({
+    _id: {type: GraphQLString},
+    title: {type: GraphQLString},
+    category: {type: Category},
+    summary: {type: GraphQLString},
+    content: {type: GraphQLString},
+    timestamp: {
+      type: GraphQLFloat,
+      resolve: function(post) {
+        if(post.date) {
+          return new Date(post.date['$date']).getTime();
+        } else {
+          return null;
+        }
+      }
+    },
+    comments: {type: new GraphQLList(Comment)},
+    author: {
+      type: Author,
+      resolve: function({author}) {
+        return AuthorsMap[author];
+      }
+    }
+  })
+});
+
+const Schema = new GraphQLSchema({
   query: new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-      count: {
-        type: GraphQLInt,
-        resolve: function() {
-          return 8000;
+    name: 'Blog',
+    description: "Root of the Blog Schema",
+    fields: () => ({
+      getPosts: {
+        type: new GraphQLList(Post),
+        description: "Get a list of posts in the blog",
+        args: {
+          category: {type: Category}
+        },
+        resolve: function(prev, {category}) {
+          if(category) {
+            return _.filter(PostsList, post => post.category === category);
+          } else {
+            return PostsList;
+          }
         }
       },
 
-      count2: {
-        type: GraphQLInt,
-        resolve: function() {
-          return 8000;
+      getRecentPosts: {
+        type: new GraphQLList(Post),
+        description: "Get recent posts in the blog",
+        args: {
+          count: {type: GraphQLInt, description: 'Number of recent items'}
         },
-        description: "This is count 2"
+        resolve: function(_, {count}) {
+          return []
+        }
+      },
+
+      getPostById: {
+        type: Post,
+        description: "Get a post by it's _id",
+        args: {
+          _id: {type: GraphQLString}
+        },
+        resolve: function(_, {_id}) {
+          return null
+        }
       }
-    }
+    })
   })
 });
 
