0f7ddfd767c9f29d139ea412dda8933827e2a663
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-10-07 15:57:17 +0530

    Make it ready to write the schema

--- src/data/authors.js
+++ src/data/authors.js
@@ -1,19 +1,24 @@
-const AuthorsMap = {
-  "arunoda": {
+const AuthorsList = [
+  {
     _id: "arunoda", 
     name: "Arunoda Susiripala",
     twitterHandle: "@arunoda"
   },
-  "pahan": {
+  {
     _id: "pahan", 
     name: "Pahan Sarathchandra",
     twitterHandle: "@pahans"
   },
-  "indi": {
+  {
     _id: "indi", 
     name: "Kasun Indi",
     twitterHandle: "@indi"
+  },
+  {
+    _id: "silva",
+    name: "Kuma Silva",
+    twitterHandle: "@silva"
   }
-};
+];
 
-export default AuthorsMap;
+export default AuthorsList;
--- src/schema.js
+++ src/schema.js
@@ -1,250 +1,51 @@
 import * as _ from 'underscore';
 
+// This is the Dataset in our blog
 import PostsList from './data/posts';
-import AuthorsMap from './data/authors';
+import AuthorsList from './data/authors';
 import {CommentList, ReplyList} from './data/comments';
 
 import {
-  GraphQLList,
-  GraphQLObjectType,
-  GraphQLSchema,
-  GraphQLString,
+  // These are the basic GraphQL types
   GraphQLInt,
   GraphQLFloat,
+  GraphQLString,
+  GraphQLList,
+  GraphQLObjectType,
   GraphQLEnumType,
-  GraphQLNonNull
+
+  // This is used to create required fileds and arguments
+  GraphQLNonNull,
+
+  // This is the class we need to create the schema
+  GraphQLSchema,
 } from 'graphql';
 
-const Category = new GraphQLEnumType({
-  name: "Category",
-  description: "A Category of the blog",
-  values: {
-    METEOR: {value: "meteor"},
-    PRODUCT: {value: "product"},
-    USER_STORY: {value: "user-story"},
-    OTHER: {value: 'other'}
-  }
-});
+/**
+  DEFINE YOUR TYPES HERE
+**/
 
-const Author = new GraphQLObjectType({
-  name: "Author",
-  description: "Represent the type of an author of a blog post or a comment",
-  fields: () => ({
-    _id: {type: GraphQLString},
-    name: {type: GraphQLString},
-    twitterHandle: {type: GraphQLString}
-  })
-});
-
-const Comment = new GraphQLObjectType({
-  name: "Comment",
-  description: "Represent the type of a comment",
-  fields: () => ({
-    _id: {type: GraphQLString},
-    content: {type: GraphQLString},
-    author: {
-      type: Author,
-      resolve: function({author}) {
-        return AuthorsMap[author];
-      }
-    },
-    timestamp: {type: GraphQLFloat},
-    replies: {
-      type: new GraphQLList(Comment),
-      description: "Replies for the comment",
-      resolve: function() {
-        return ReplyList;
-      }
-    }
-  })
-});
-
-const Post = new GraphQLObjectType({
-  name: "Post",
-  description: "Represent the type of a blog post",
-  fields: () => ({
-    _id: {type: GraphQLString},
-    title: {type: GraphQLString},
-    category: {type: Category},
-    summary: {type: GraphQLString},
-    content: {type: GraphQLString},
-    timestamp: {
-      type: GraphQLFloat,
-      resolve: function(post) {
-        if(post.date) {
-          return new Date(post.date['$date']).getTime();
-        } else {
-          return null;
-        }
-      }
-    },
-    comments: {
-      type: new GraphQLList(Comment),
-      args: {
-        limit: {type: GraphQLInt, description: "Limit the comments returing"}
-      },
-      resolve: function(post, {limit}) {
-        if(limit >= 0) {
-          return CommentList.slice(0, limit);
-        }
-
-        return CommentList;
-      }
-    },
-    author: {
-      type: Author,
-      resolve: function({author}) {
-        return AuthorsMap[author];
-      }
-    }
-  })
-});
-
+// This is the Root Query
 const Query = new GraphQLObjectType({
   name: 'BlogQueries',
   description: "Root of the Blog Schema",
   fields: () => ({
-    getPosts: {
-      type: new GraphQLList(Post),
-      description: "Get a list of posts in the blog",
+    echo: {
+      type: GraphQLString,
+      description: "Echo what you enter",
       args: {
-        category: {type: Category}
+        message: {type: GraphQLString}
       },
-      resolve: function(source, {category}) {
-        if(category) {
-          return _.filter(PostsList, post => post.category === category);
-        } else {
-          return PostsList;
-        }
-      }
-    },
-
-    getLatestPost: {
-      type: Post,
-      description: "Get the latest post in the blog",
-      resolve: function() {
-        PostsList.sort((a, b) => {
-          var bTime = new Date(b.date['$date']).getTime();
-          var aTime = new Date(a.date['$date']).getTime();
-
-          return bTime - aTime;
-        });
-
-        return PostsList[0];
-      }
-    },
-
-    getRecentPosts: {
-      type: new GraphQLList(Post),
-      description: "Get recent posts in the blog",
-      args: {
-        count: {type: new GraphQLNonNull(GraphQLInt), description: 'Number of recent items'}
-      },
-      resolve: function(source, {count}) {
-        PostsList.sort((a, b) => {
-          var bTime = new Date(b.date['$date']).getTime();
-          var aTime = new Date(a.date['$date']).getTime();
-
-          return bTime - aTime;
-        });
-
-        return PostsList.slice(0, count)
-      }
-    },
-
-    getPostById: {
-      type: Post,
-      description: "Get a post by it's _id",
-      args: {
-        _id: {type: GraphQLString}
-      },
-      resolve: function(source, {_id}) {
-        return _.filter(PostsList, post => post._id === _id)[0];
-      }
-    },
-
-    getAuthors: {
-      type: new GraphQLList(Author),
-      description: "Get the available authors in the blog",
-      resolve: function() {
-        return _.values(AuthorsMap);
-      }
-    },
-
-    getAuthorById: {
-      type: Author,
-      description: "Get the author by given author id",
-      args: {
-        _id: {type: new GraphQLNonNull(GraphQLString)}
-      },
-      resolve: function(source, {_id}) {
-        return AuthorsMap[_id];
+      resolve: function(source, {message}) {
+        return `recieved ${message}`;
       }
     }
   })
 });
 
-const Mutation = new GraphQLObjectType({
-  name: "BlogMutations",
-  fields: {
-    createPost: {
-      type: Post,
-      description: "Create a new blog post",
-      args: {
-        _id: {type: new GraphQLNonNull(GraphQLString)},
-        title: {type: new GraphQLNonNull(GraphQLString)},
-        content: {type: new GraphQLNonNull(GraphQLString)},
-        summary: {type: GraphQLString},
-        category: {type: Category},
-        author: {type: new GraphQLNonNull(GraphQLString), description: "Id of the author"}
-      },
-      resolve: function(source, args) {
-        let post = _.clone(args);
-        var alreadyExists = _.findIndex(PostsList, p => p._id === post._id) >= 0;
-        if(alreadyExists) {
-          throw new Error("Post already exists: " + post._id);
-        }
-
-        if(!AuthorsMap[post.author]) {
-          throw new Error("No such author: " + post.author);
-        }
-
-        if(!post.summary) {
-          post.summary = post.content.substring(0, 100);
-        }
-
-        post.comments = [];
-        post.date = {$date: new Date().toString()}
-
-        PostsList.push(post);
-        return post;
-      }
-    },
-
-    createAuthor: {
-      type: Author,
-      description: "Create a new author",
-      args: {
-        _id: {type: new GraphQLNonNull(GraphQLString)},
-        name: {type: new GraphQLNonNull(GraphQLString)},
-        twitterHandle: {type: GraphQLString}
-      },
-      resolve: function(source, args) {
-        let author = _.clone(args);
-        if(AuthorsMap[args._id]) {
-          throw new Error("Author already exists: " + author._id);
-        }
-
-        AuthorsMap[author._id] = author;
-        return author;
-      }
-    }
-  }
-});
-
+// This the Schema
 const Schema = new GraphQLSchema({
-  query: Query,
-  mutation: Mutation
+  query: Query
 });
 
 export default Schema;
