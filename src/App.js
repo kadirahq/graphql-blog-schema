105ec1136920d1dc1368578fedcccbaf56eaedfa
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-10-13 14:08:03 +0530

    Change logo

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import React, { Component } from 'react';
 import GraphiQL from 'graphiql';
 import fetch from 'isomorphic-fetch';
 import Schema from './schema.js';
-window.a = require('graphql');
+
+GraphiQL.Logo = class Logo extends Component {
+  render() {
+    let style = {
+      fontWeight: 800,
+      fontSize: 16,
+      color: "#252525"
+    };
+
+    return (
+      <span style={style}>Learn GraphQL Sandbox</span>
+    );
+  }
+}
 
 export default class App extends Component {
   fetchData({query, variables}) {
