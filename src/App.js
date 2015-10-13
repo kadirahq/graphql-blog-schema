f56f59d0fea96a51dbeb0a5ced000fba9d5ea7c3
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-10-13 14:17:37 +0530

    Fix some issues after changing the logo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import GraphiQL from 'graphiql';
 import fetch from 'isomorphic-fetch';
 import Schema from './schema.js';
+import { graphql } from 'graphql';
 
 GraphiQL.Logo = class Logo extends Component {
   render() {
@@ -19,7 +20,11 @@ GraphiQL.Logo = class Logo extends Component {
 
 export default class App extends Component {
   fetchData({query, variables}) {
-    return window.a.graphql(Schema, query, null, JSON.parse(variables));
+    let queryVariables = {};
+    try {
+      queryVariables = JSON.parse(variables);
+    } catch(ex) {}
+    return graphql(Schema, query, null, queryVariables);
   }
 
   render() {
